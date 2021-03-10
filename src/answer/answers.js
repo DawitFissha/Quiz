fce66a2a0a27dc253ca8d1c8ca354ede745e35f6
Dawit_Fissha <dawitfissha1@gmail.com>
2021-03-10 00:58:04 -0800

    import of answers/boolean.js removed from answers/answers.js

--- src/answer/answers.js
+++ src/answer/answers.js
@@ -2,7 +2,7 @@ import {Button,Box,Typography} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import {useRef,useState,useEffect} from 'react';
 import Result from './result'
-import Boolean from './boolean'
+
 const useStyles = makeStyles({
     button: {
       //width: '100%',
